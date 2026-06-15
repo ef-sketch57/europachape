3aa7a9427cff7a0c10f956c21bb830095fdc5687
ef-sketch57 <288275544+ef-sketch57@users.noreply.github.com>
2026-06-15 12:33:39 +0200

    Update and rename config.sample.php to config.php

--- includes/config.php
+++ includes/config.php
@@ -17,10 +17,10 @@ return [
     //  IONOS dans votre espace client (ex. db5012345678.hosting-data.io).
     // ---------------------------------------------------------------
     'db' => [
-        'host'    => 'VOTRE_HOTE_IONOS',     // ex. db1234567890.hosting-data.io
-        'name'    => 'VOTRE_NOM_DE_BASE',    // ex. dbs1234567
-        'user'    => 'VOTRE_UTILISATEUR',    // ex. dbu1234567
-        'pass'    => 'VOTRE_MOT_DE_PASSE',
+        'host'    => 'db517208864.db.1and1.com',     // ex. db1234567890.hosting-data.io
+        'name'    => 'dbo517208864',    // ex. dbs1234567
+        'user'    => 'dbo517208864',    // ex. dbu1234567
+        'pass'    => 'Europachape2026!',
         'charset' => 'utf8mb4',
     ],
 
